7b2c301b7de0cde7db3d2a1cd52c45a42bcdd6ac
pinojun <pinojun67@gmail.com>
2025-04-26 08:57:29 +0900

    eleventh commit

--- lib/screens/zoom/zoom_image.dart
+++ lib/screens/zoom/zoom_image.dart
@@ -17,43 +17,43 @@ class ZoomedImagePage extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(10.0),
           child: Center(
-            child: Hero(
-              tag: "anatomy_${product.id}",
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  ClipRRect(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Hero(
+                  tag: "anatomy_${product.id}",
+                  child: ClipRRect(
                     borderRadius: BorderRadius.circular(15),
                     child: Image.asset(
                       product.anaimage,
                       fit: BoxFit.fill,
                     ),
                   ),
-                  const SizedBox(height: 20),
-                  const Text(
-                    ' Check Point',
-                    style: Title2,
-                  ),
-                  const SizedBox(height: 2),
-                  Container(
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        border: Border.all(color: Colors.blue, width: 1),
-                        boxShadow: const [
-                          BoxShadow(color: Colors.blue, blurRadius: 3)
-                        ],
-                        borderRadius: BorderRadius.circular(12)),
-                    child: Container(
-                      padding: const EdgeInsets.all(10),
-                      width: MediaQuery.of(context).size.width,
-                      child: Text(
-                        product.checkpoint,
-                        style: Description1,
-                      ),
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  ' Check Point',
+                  style: Title2,
+                ),
+                const SizedBox(height: 2),
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(color: Colors.blue, width: 1),
+                      boxShadow: const [
+                        BoxShadow(color: Colors.blue, blurRadius: 3)
+                      ],
+                      borderRadius: BorderRadius.circular(12)),
+                  child: Container(
+                    padding: const EdgeInsets.all(10),
+                    width: MediaQuery.of(context).size.width,
+                    child: Text(
+                      product.checkpoint,
+                      style: Description1,
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
